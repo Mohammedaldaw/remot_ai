46cc4d4b1759cab3f440094559051b638120fb15
Mohammedaldaw <mohammedaldawood43@gmail.com>
2025-07-14 19:50:22 +0300

    read and visualize

--- main.py
+++ main.py
@@ -6,11 +6,14 @@ def main():
     df = pd.read_csv("CAR DETAILS FROM CAR DEKHO.csv")
     print("First 5 rows:\n",df.head())
 
-df.plot(kind="bar", x="name", y="score", legend=False, color="skyblue")
-plt.title("Student Scores")
-plt.ylabel("Score")
-plt.tight_layout()
-plt.show()
+    plt.figure(figsize=(12, 5))
+    plt.plot(df["selling_price"].values, color="green")
+    plt.title("Used Car Selling Prices")
+    plt.xlabel("Car Index")
+    plt.ylabel("Selling Price")
+    plt.grid(True)
+    plt.tight_layout()
+    plt.show()
 
 if __name__ == "__main__":
     main()
