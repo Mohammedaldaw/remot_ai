7b6402dbacd81bc3a63d13f17a724528d3567667
Mohammedaldaw <mohammedaldawood43@gmail.com>
2025-07-14 19:35:20 +0300

    doing read and plot

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-def main():
-    print("Hello from ai!")
+import pandas as pd 
+import matplotlib.pyplot as plt 
 
+
+def main():
+    df = pd.read_csv("CAR DETAILS FROM CAR DEKHO.csv")
+    print("First 5 rows:\n",df.head())
+
+df.plot(kind="bar", x="name", y="score", legend=False, color="skyblue")
+plt.title("Student Scores")
+plt.ylabel("Score")
+plt.tight_layout()
+plt.show()
 
 if __name__ == "__main__":
     main()
